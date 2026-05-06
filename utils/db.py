78a14142185731d3f35ebdc86f140a9ae47ba6a8
student-Vg <gudhialvikram@gmail.com>
2026-05-06 23:54:03 +0530

    Fix MongoDB URI normalization for special characters

--- utils/db.py
+++ utils/db.py
@@ -1,20 +1,36 @@
 from pymongo import MongoClient
 import gridfs
 import os
-from urllib.parse import quote_plus
+from urllib.parse import quote_plus, unquote_plus, urlsplit, urlunsplit
 
 # Get MONGO_URI from environment, or construct it from components
 MONGO_URI = os.getenv('MONGO_URI', None)
 
-# If MONGO_URI not set, try to build from components
-if not MONGO_URI:
+# If MONGO_URI is present, normalize it by encoding username/password safely.
+def normalize_mongo_uri(uri: str) -> str:
+    parsed = urlsplit(uri)
+    if not parsed.netloc or '@' not in parsed.netloc:
+        return uri
+
+    userinfo, hostinfo = parsed.netloc.rsplit('@', 1)
+    if ':' not in userinfo:
+        return uri
+
+    user, password = userinfo.split(':', 1)
+    user = quote_plus(unquote_plus(user))
+    password = quote_plus(unquote_plus(password))
+    normalized_netloc = f"{user}:{password}@{hostinfo}"
+    return urlunsplit((parsed.scheme, normalized_netloc, parsed.path, parsed.query, parsed.fragment))
+
+if MONGO_URI:
+    MONGO_URI = normalize_mongo_uri(MONGO_URI)
+else:
     mongo_user = os.getenv('MONGO_USER', 'placement_user')
     mongo_password = os.getenv('MONGO_PASSWORD', '')
     mongo_host = os.getenv('MONGO_HOST', 'localhost')
     mongo_port = os.getenv('MONGO_PORT', '27017')
-    
+
     if mongo_password:
-        # URL-encode password to handle special characters
         encoded_password = quote_plus(mongo_password)
         MONGO_URI = f'mongodb+srv://{mongo_user}:{encoded_password}@{mongo_host}/?retryWrites=true&w=majority'
     else:
